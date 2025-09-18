9c68ddc6bfdd99cf5eb4f9bf0cef96cb33d24d66
Vivek Kumar <vivekkumar75199@gmail.com>
2025-09-18 12:18:11 +0530

    334

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,62 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controllers.js"
-import {validate} from "../middlewares/validator.middleware.js"
-import { userRegistrationValidator } from"../validator/index.js";
+import {
+    forgotPasswordRequest,
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    resendEmailVerification,
+    resetForgottenPassword,
+    verifyEmail,
+    changeCurrentPassword,
+} from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+    passwordValidator,
+    emailValidator,
+    userLoginValidator,
+    userRegistrationValidator,
+    changePasswordValidator,
+} from "../validators/index.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
+const router = Router();
+import userDetailsRoutes from "./user.routes.js";
 
+router.use("/user-details", userDetailsRoutes);
 
+router
+    .route("/register")
+    .post(userRegistrationValidator(), validate, registerUser);
 
+router.route("/verify-email").get(verifyEmail);
+router
+    .route("/resend-verification-email")
+    .post(emailValidator(), validate, resendEmailVerification);
 
-const router=Router() 
+router.route("/login").post(userLoginValidator(), validate, loginUser);
 
-router.route("/register").post(userRegistrationValidator(),validate,registerUser);
+router.route("/logout").get(verifyToken, logoutUser);
+router
+    .route("/forgot-password-request")
+    .post(emailValidator(), validate, forgotPasswordRequest);
 
+router
+    .route("/reset-password")
+    .post(passwordValidator(), validate, resetForgottenPassword);
 
-export default router
+router.route("/refresh-access-token").post(refreshAccessToken);
+
+router.route("/current-user").get(verifyToken, getCurrentUser);
+
+router
+    .route("/change-password")
+    .patch(
+        changePasswordValidator(),
+        validate,
+        verifyToken,
+        changeCurrentPassword,
+    );
+
+export default router;
