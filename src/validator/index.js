370ae848859d12d4af286afb0d14d10c81f51208
Vivek Kumar <vivekkumar75199@gmail.com>
2025-09-19 19:54:27 +0530

    note.routes.js

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,35 +1,3 @@
-// import {body} from "express-validator"
-
-// const userRegistrationValidator=()=>{
-    
-//     return [
-//         body('email')
-//         .trim()
-//         .notEmpty().withMessage("Email is required")
-//         .isEmail.withMessage("Email is invalid"),
-//         body("username")
-//         .trim()
-//         .notEmpty().withMessage("username is required")
-//         .isLength({min:3}).withMessage("username should be ar least 3 char")
-//         .isLength({max:13}).withMessage("username cant exceed 13 char")
-
-//     ]
-// }
-
-// export const userLoginValidator=()=>{
-//     return [
-//         body("email").isEmail.withMessage("Email is not valid"),
-//         body("password").notEmpty().withMessage("password cannot be empty"),
-//     ];
-// }
-
-// // export {userRegistrationValidator}
-
-
-
-
-
-
 import { body, param } from "express-validator";
 import {
     AvailableUserRoles,
